a7c694ac746838a31b4b923d4cda6b089811e6a2
Shahzaman-Mazumder-Dihan <shahzamanmazumder@gmail.com>
2024-02-22 00:27:41 +0600

    last

--- js/showSuccessPage.js
+++ js/showSuccessPage.js
@@ -5,7 +5,6 @@ function successMessage() {
     alert('Name and Phone Number are required!');
     return false; // Prevent form submission
   }else{
-    document.getElementById('success-msg-container').classList.remove('hidden');
-    document.getElementById('success-msg-container').classList.add('flex');
+   
   }
 }
